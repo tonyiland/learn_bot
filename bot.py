7f00f7a396870d7a9aef815f35b4d84dab5c1b2b
Anatoliy Ostrovko <ostrovko01@mail.ru>
2022-09-07 21:55:32 +0400

    New shiny bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings 
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def greet_user(update, context):
+    print('Вызыван /start') 
+    update.message.reply_text('Здравствуйте!')
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
+def main():
+    mybot = Updater(settings.API_KEY , use_context = True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info('BOT STARTED')
+
+    mybot.start_polling()
+
+    mybot.idle()
+if __name__ == "__main__":
+    main()
